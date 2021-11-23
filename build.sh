328d0dab4ee3860e47e3ee9ac86fa26a01401538
Anthony McClosky <143364+amcclosky@users.noreply.github.com>
2021-11-23 23:56:16 +0000

    add cleaner dependency install

--- build.sh
+++ build.sh
@@ -4,8 +4,17 @@ set -e
 echo "Install a recent version of pip"
 pip install --upgrade pip
 
-pip install pipx
-pipx install supervisor
+export TOOLS_VENV=./.tools
+python -m venv ${TOOLS_VENV}
+
+export PIPX_HOME=./.pipx
+export PIPX_BIN_DIR=./.tools-bin/bin
+
+mkdir -p ${PIPX_HOME}
+mkdir -p ${PIPX_BIN_DIR}
+
+${TOOLS_VENV}/bin/pip install pipx
+${PIPX_BIN_DIR}/pipx install supervisor
 
 echo "Install depenencies w/ poetry"
 pip install -r requirements.txt
