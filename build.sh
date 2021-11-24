d44e14485cb6d798ed145c0dd1cd22ed2e1bc87e
Anthony McClosky <143364+amcclosky@users.noreply.github.com>
2021-11-24 00:08:14 +0000

    add logging and comments

--- build.sh
+++ build.sh
@@ -4,16 +4,21 @@ set -e
 echo "Install a recent version of pip"
 pip install --upgrade pip
 
+echo "Install pipx"
 export TOOLS_VENV=./.tools
 python -m venv ${TOOLS_VENV}
 
 export PIPX_HOME=./.pipx
-export PIPX_BIN_DIR=./.tools-bin/bin
+export PIPX_BIN_DIR=./.local/bin
 
+# ensure the pipx directories exist
 mkdir -p ${PIPX_HOME}
 mkdir -p ${PIPX_BIN_DIR}
 
 ${TOOLS_VENV}/bin/pip install pipx
+
+# Use pipx to install supervisor to an isolated venv
+echo "Install supervisord"
 ${TOOLS_VENV}/bin/pipx install supervisor
 
 echo "Install depenencies w/ poetry"
